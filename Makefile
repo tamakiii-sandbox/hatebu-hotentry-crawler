0e7d9f87a59ec6e4d2ecc17cc5a9fa9dce3f93cd
tamakiii <tamakiii@users.noreply.github.com>
2019-11-15 01:05:05 +0900

    fix

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
+SHELL := bash
 
 install:
 	npm install
 
+hotentry/all/%:
+	make -f make/html.mk dist/hotentry/all/$(@F).html
+	make -f make/parse.mk dist/hotentry/all/$(@F).json
+
 clean:
 	rm -rf node_modules
+	rm -rf dist
 
--- make/html.mk
+++ make/html.mk
@@ -1,10 +1,12 @@
+SHELL := bash
+
 TARGET := $(shell date "+%Y%m%d")
 
 all: dist/hotentry/all/$(TARGET).html
 
 dist/hotentry/all/%: dist/hotentry/all
 	echo $(@F) | grep '^[1-9][0-9]\{3\}[0-9]\{2\}[0-9]\{2\}.html' || exit 1
-	curl "https://b.hatena.ne.jp/hotentry/all/$(subst .html,,$(@F))" > $@
+	curl -s "https://b.hatena.ne.jp/hotentry/all/$(subst .html,,$(@F))" > $@
 
 dist:
 	mkdir $@
