30dd28d7894c504d5fc4038df14e6c1daf10fa65
tamakiii <tamakiii@users.noreply.github.com>
2019-11-15 16:44:12 +0900

    mk -> md

--- make/markdown.mk
+++ make/markdown.mk
@@ -3,8 +3,8 @@ SHELL := bash
 DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 
 dist/hotentry/all/%:
-	echo $(@F) | grep '^[1-9][0-9]\{3\}[0-9]\{2\}.mk' || exit 1
+	echo $(@F) | grep '^[1-9][0-9]\{3\}[0-9]\{2\}.md' || exit 1
 	ls dist/hotentry/all/*.json | grep '^dist/hotentry/all/$(subst .mk,,$(@F))' | xargs -I@ node $(DIR)/src/2md.js '@' > $@
 
 clean:
-	rm dist/hotentry/all/*.mk
+	rm dist/hotentry/all/*.md
