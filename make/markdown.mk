62ac1ec6983505e4b1e298af8cb027b28576a557
tamakiii <tamakiii@users.noreply.github.com>
2019-11-15 17:07:30 +0900

    fix mk -> md

--- make/markdown.mk
+++ make/markdown.mk
@@ -4,15 +4,15 @@ DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 
 dist/hotentry/all/%:
 	echo $(@F) | grep '^[1-9][0-9]\{3\}[0-9]\{2\}.md' || exit 1
-	ls dist/hotentry/all/*.json | grep '^dist/hotentry/all/$(subst .mk,,$(@F))' | xargs -I@ node $(DIR)/src/2md.js '@' > $@
+	ls dist/hotentry/all/*.json | grep '^dist/hotentry/all/$(subst .md,,$(@F))' | xargs -I@ node $(DIR)/src/2md.js '@' > $@
 
 dist/hotentry/economics/%:
 	echo $(@F) | grep '^[1-9][0-9]\{3\}[0-9]\{2\}.md' || exit 1
-	ls dist/hotentry/economics/*.json | grep '^dist/hotentry/economics/$(subst .mk,,$(@F))' | xargs -I@ node $(DIR)/src/2md.js '@' > $@
+	ls dist/hotentry/economics/*.json | grep '^dist/hotentry/economics/$(subst .md,,$(@F))' | xargs -I@ node $(DIR)/src/2md.js '@' > $@
 
 dist/hotentry/social/%:
 	echo $(@F) | grep '^[1-9][0-9]\{3\}[0-9]\{2\}.md' || exit 1
-	ls dist/hotentry/social/*.json | grep '^dist/hotentry/social/$(subst .mk,,$(@F))' | xargs -I@ node $(DIR)/src/2md.js '@' > $@
+	ls dist/hotentry/social/*.json | grep '^dist/hotentry/social/$(subst .md,,$(@F))' | xargs -I@ node $(DIR)/src/2md.js '@' > $@
 
 clean:
 	rm dist/hotentry/all/*.md
